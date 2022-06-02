5db230cae3d7ddeae898381ba7fab1724f092c7f
MiguelX413 <lbpgaming27@gmail.com>
2022-06-02 15:59:59 -0700

    Remove sum_as_string(), add utf16len(), add miguel_lib.pyi

---
+++ miguel_lib.pyi
@@ -0,0 +1,2 @@
+def utf16len(string: str) -> int:
+    """Returns the UTF-16 length of a string."""
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
-
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+/// Returns the UTF-16 length of a string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn utf16len(string: String) -> usize {
+    return string.encode_utf16().count();
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn miguel_lib(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(utf16len, m)?)?;
     Ok(())
 }

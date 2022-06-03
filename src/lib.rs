7c0a04596ea74a4531e7eb4b126fbc53cf83fae0
MiguelX413 <lbpgaming27@gmail.com>
2022-06-03 01:19:43 -0700

    Changed utf16len() algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@ use pyo3::prelude::*;
 /// Returns the UTF-16 length of a string.
 #[pyfunction]
 fn utf16len(string: &str) -> usize {
-    return string.encode_utf16().count();
+    let mut length = 0;
+    for char in string.chars() {
+        length += char.len_utf16();
+    }
+    return length;
 }
 
 /// A Python module implemented in Rust.

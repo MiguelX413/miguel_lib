62034fa1ecf6108a8f5a39b244bc4cab2e562df9
MiguelX413 <lbpgaming27@gmail.com>
2022-06-03 10:16:03 -0700

    Optimize Interval.__repr__()

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,11 @@ impl Interval {
         Interval { _intervals: value }
     }
     fn __repr__(&self) -> String {
-        let mut output = "Interval([".to_string();
         let mut intervals: Vec<String> = vec![];
         for interval in self._intervals.iter() {
             intervals.push(format!("({}, {})", interval.0, interval.1));
         }
-        output.push_str(&*intervals.join(", "));
-        output.push_str("])");
-        return output;
+        return format!("Interval([{}])", intervals.join(", "));
     }
 }
 

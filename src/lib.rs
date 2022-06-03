23041245f4e0d9bb42cc1b8e2a1b482a9a0c7b23
MiguelX413 <lbpgaming27@gmail.com>
2022-06-03 10:09:06 -0700

    Add Interval.__repr__()

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,16 @@ impl Interval {
     fn new(value: Vec<(isize, isize)>) -> Self {
         Interval { _intervals: value }
     }
+    fn __repr__(&self) -> String {
+        let mut output = "Interval([".to_string();
+        let mut intervals: Vec<String> = vec![];
+        for interval in self._intervals.iter() {
+            intervals.push(format!("({}, {})", interval.0, interval.1));
+        }
+        output.push_str(&*intervals.join(", "));
+        output.push_str("])");
+        return output;
+    }
 }
 
 

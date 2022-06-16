3d48a975ace6fb92862b2207fab09d704a8ac6be
MiguelX413 <lbpgaming27@gmail.com>
2022-06-15 17:04:15 -0700

    Rename Interval to Span

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,18 @@ use std::cmp::max;
 use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 
-fn merge_intervals(intervals: &mut Vec<(i32, i32)>) {
-    intervals.sort_by_key(|&a| a.0);
+fn merge_sub_spans(sub_spans: &mut Vec<(i32, i32)>) {
+    sub_spans.sort_by_key(|&a| a.0);
     let mut index: usize = 0;
-    for i in 1..intervals.len() {
-        if intervals[index].1 >= intervals[i].0 {
-            intervals[index].1 = max(intervals[index].1, intervals[i].1);
+    for i in 1..sub_spans.len() {
+        if sub_spans[index].1 >= sub_spans[i].0 {
+            sub_spans[index].1 = max(sub_spans[index].1, sub_spans[i].1);
         } else {
             index += 1;
-            intervals[index] = intervals[i];
+            sub_spans[index] = sub_spans[i];
         }
     }
-    intervals.truncate(index + 1);
+    sub_spans.truncate(index + 1);
 }
 
 /// Returns a list of the UTF-8 indices of disjoint matches, from start to end.
@@ -112,53 +112,53 @@ fn utf16len(string: &str) -> usize {
     string.chars().map(|char| char.len_utf16()).sum()
 }
 
-/// A class used to represent intervals.
+/// A class used to represent spans.
 #[pyclass]
-struct Interval {
-    intervals: Vec<(i32, i32)>,
+struct Span {
+    sub_spans: Vec<(i32, i32)>,
 }
 
 #[pymethods]
-impl Interval {
+impl Span {
     #[new]
-    fn py_new(interval_list: Option<Vec<(i32, i32)>>) -> PyResult<Self> {
-        match interval_list {
+    fn py_new(sub_spans: Option<Vec<(i32, i32)>>) -> PyResult<Self> {
+        match sub_spans {
             Some(mut f) => {
-                if f.iter().any(|&subinterval| subinterval.0 > subinterval.1) {
+                if f.iter().any(|&subspan| subspan.0 > subspan.1) {
                     Err(PyValueError::new_err(
-                        "Start point of sub-interval cannot be greater than its end point",
+                        "Start point of sub-span cannot be greater than its end point",
                     ))
                 } else {
-                    merge_intervals(&mut f);
-                    Ok(Interval { intervals: f })
+                    merge_sub_spans(&mut f);
+                    Ok(Span { sub_spans: f })
                 }
             }
-            None => Ok(Interval { intervals: vec![] }),
+            None => Ok(Span { sub_spans: vec![] }),
         }
     }
     #[args(other = "*")]
-    fn union(&self, other: &PyTuple) -> PyResult<Interval> {
+    fn union(&self, other: &PyTuple) -> PyResult<Span> {
         let mut output = self.clone();
         output.union_update(other)?;
         Ok(output)
     }
     #[args(other = "*")]
     fn union_update(&mut self, other: &PyTuple) -> PyResult<()> {
-        let inputs: Vec<Interval> = other.extract()?;
-        self.intervals
-            .extend(inputs.iter().flat_map(|f| &f.intervals));
+        let inputs: Vec<Span> = other.extract()?;
+        self.sub_spans
+            .extend(inputs.iter().flat_map(|f| &f.sub_spans));
         if !inputs.is_empty() {
-            merge_intervals(&mut self.intervals);
+            merge_sub_spans(&mut self.sub_spans);
         }
         Ok(())
     }
     fn __contains__(&self, item: i32) -> bool {
-        self.intervals.iter().any(|&f| f.0 <= item && item <= f.1)
+        self.sub_spans.iter().any(|&f| f.0 <= item && item <= f.1)
     }
     fn __repr__(&self) -> String {
         format!(
-            "Interval([{}])",
-            self.intervals
+            "Span([{}])",
+            self.sub_spans
                 .iter()
                 .map(|&f| format!("({}, {})", f.0, f.1))
                 .collect::<Vec<String>>()
@@ -168,28 +168,28 @@ impl Interval {
     fn __str__(&self) -> String {
         format!(
             "({})",
-            self.intervals
+            self.sub_spans
                 .iter()
                 .map(|&f| format!("[{}, {}]", f.0, f.1))
                 .collect::<Vec<String>>()
                 .join(" ∪ ")
         )
     }
-    fn __or__(&self, other: &Interval) -> Interval {
+    fn __or__(&self, other: &Span) -> Span {
         let mut output = self.clone();
         output.__ior__(other);
         output
     }
-    fn __ior__(&mut self, other: &Interval) {
-        self.intervals.append(&mut other.intervals.clone());
-        merge_intervals(&mut self.intervals);
+    fn __ior__(&mut self, other: &Span) {
+        self.sub_spans.append(&mut other.sub_spans.clone());
+        merge_sub_spans(&mut self.sub_spans);
     }
 }
 
-impl Clone for Interval {
-    fn clone(&self) -> Interval {
-        Interval {
-            intervals: self.intervals.clone(),
+impl Clone for Span {
+    fn clone(&self) -> Span {
+        Span {
+            sub_spans: self.sub_spans.clone(),
         }
     }
 }
@@ -204,7 +204,7 @@ fn miguel_lib(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(rmatch_utf16_indices, m)?)?;
     m.add_function(wrap_pyfunction!(rmatch_byte_indices, m)?)?;
     m.add_function(wrap_pyfunction!(utf16len, m)?)?;
-    m.add_class::<Interval>()?;
+    m.add_class::<Span>()?;
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }
